dc0ba25f5f7cc88879ba04b14074f001d409659b
zhtengw <atenzd@gmail.com>
2012-10-28 13:47:22 +0800

    enable load tranlation in different language

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,24 @@
 /***************************************************************************
  *  Copyright (c) 2012 by Aten Zhang <atenzd@gmail.com>                    *
  *                                                                         *
- *  This file is part of WarMtH.                                       *
+ *  This file is part of WarMtH.                                           *
  *                                                                         *
- *  WarMtH is free software: you can redistribute it and/or modify     *
+ *  WarMtH is free software: you can redistribute it and/or modify         *
  *  it under the terms of the GNU General Public License as published by   *
  *  the Free Software Foundation, either version 3 of the License, or      *
  *  (at your option) any later version.                                    *
  *                                                                         *
- *  WarMtH is distributed in the hope that it will be useful,          *
+ *  WarMtH is distributed in the hope that it will be useful,              *
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
  *  GNU General Public License for more details.                           *
  *                                                                         *
  *  You should have received a copy of the GNU General Public License      *
- *  along with WarMtH.  If not, see <http://www.gnu.org/licenses/>.    *
+ *  along with WarMtH.  If not, see <http://www.gnu.org/licenses/>.        *
  ***************************************************************************/
 #include <QApplication>
 #include <QTranslator>
+#include <QLibraryInfo>
 
 #include "mainwindow.h"
 
@@ -26,8 +27,14 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setWindowIcon(QIcon(":/warmth.png"));
     QTranslator qtTranslator;
-    qtTranslator.load("/usr/share/warmth/l10n/zh-CN.qm");
+    qtTranslator.load("qt_" + QLocale::system().name(),
+                      QLibraryInfo::location(QLibraryInfo::TranslationsPath));
     app.installTranslator(&qtTranslator);
+
+    QTranslator warmthTranslator;
+    warmthTranslator.load("warmth_" + QLocale::system().name(),"/usr/share/warmth/l10n");
+    app.installTranslator(&warmthTranslator);
+
     QCoreApplication::setOrganizationName("WarMtH");
     QCoreApplication::setApplicationName("warmth");
     MainWindow *mainWD = new MainWindow;
