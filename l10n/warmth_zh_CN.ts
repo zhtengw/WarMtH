00285e33d1ffbfc7d2673669a951ea95be01ac24
zhtengw <atenzd@gmail.com>
2012-10-30 21:46:53 +0800

    update Chinese translation

--- l10n/warmth_zh_CN.ts
+++ l10n/warmth_zh_CN.ts
@@ -44,169 +44,211 @@
 <context>
     <name>ConfigWindow</name>
     <message>
-        <location filename="../src/configwindow.cpp" line="30"/>
+        <location filename="../src/configwindow.cpp" line="38"/>
         <source>Confir&amp;m</source>
         <translation>确认(&amp;M)</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="31"/>
+        <location filename="../src/configwindow.cpp" line="39"/>
         <source>Canc&amp;el</source>
         <translation>取消(&amp;E)</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="41"/>
+        <location filename="../src/configwindow.cpp" line="50"/>
         <source>Netcard:</source>
         <translation>网卡:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="44"/>
         <source>Select the network card you used.</source>
-        <translation>选择你要使用的网卡。</translation>
+        <translation type="obsolete">选择你要使用的网卡。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="70"/>
+        <location filename="../src/configwindow.cpp" line="79"/>
         <source>Mulcast Address:</source>
         <translation>组播地址:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="71"/>
+        <location filename="../src/configwindow.cpp" line="80"/>
         <source>Mulcast Address</source>
         <translation>组播地址</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="75"/>
+        <location filename="../src/configwindow.cpp" line="84"/>
         <source>standard</source>
         <translation>标准</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="75"/>
+        <location filename="../src/configwindow.cpp" line="84"/>
         <source>ruijie</source>
         <translation>锐捷</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="75"/>
+        <location filename="../src/configwindow.cpp" line="84"/>
         <source>saier</source>
         <translation>赛尔</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="89"/>
+        <location filename="../src/configwindow.cpp" line="98"/>
         <source>Display Notification:</source>
         <translation>显示通知:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="90"/>
         <source>Seconds to display system notification, letters to no show.</source>
-        <translation>显示系统通知的时间，字母表示不显示。</translation>
+        <translation type="obsolete">显示系统通知的时间，字母表示不显示。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="93"/>
+        <location filename="../src/configwindow.cpp" line="33"/>
+        <source>&amp;Arguments</source>
+        <translation>参数设定(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="34"/>
+        <source>A&amp;pplication</source>
+        <translation>程序设定(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="53"/>
+        <source>Select the network card you used</source>
+        <translation>选择你要使用的网卡</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="99"/>
+        <source>Seconds to display system notification, letters to no show</source>
+        <translation>显示系统通知的时间，字母表示不显示</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="102"/>
         <source>s [0(no) 1-20(yes)]</source>
         <translation>秒[0(不显示) 1-20(显示)]</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="114"/>
+        <location filename="../src/configwindow.cpp" line="123"/>
         <source>DHCP type:</source>
         <translation>DHCP方式:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="115"/>
         <source>Set the type of DHCP.</source>
-        <translation>设置DHCP的方式。</translation>
+        <translation type="obsolete">设置DHCP的方式。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="119"/>
+        <location filename="../src/configwindow.cpp" line="124"/>
+        <source>Set the type of DHCP</source>
+        <translation>设置DHCP的方式</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="128"/>
         <source>not in used</source>
         <translation>不使用</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="119"/>
+        <location filename="../src/configwindow.cpp" line="128"/>
         <source>secondary authenticate</source>
         <translation>二次认证</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="119"/>
+        <location filename="../src/configwindow.cpp" line="128"/>
         <source>post authenticate</source>
         <translation>认证后</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="119"/>
+        <location filename="../src/configwindow.cpp" line="128"/>
         <source>pre authenticate</source>
         <translation>认证前</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="133"/>
+        <location filename="../src/configwindow.cpp" line="142"/>
         <source>Authenticate timeout:</source>
         <translation>认证超时:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="134"/>
-        <source>Seconds to wait for authentication.</source>
-        <translation>等待认证完成的时间。</translation>
+        <location filename="../src/configwindow.cpp" line="143"/>
+        <source>Seconds to wait for authentication</source>
+        <translation>等待认证完成的时间</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="137"/>
-        <location filename="../src/configwindow.cpp" line="182"/>
-        <location filename="../src/configwindow.cpp" line="206"/>
+        <location filename="../src/configwindow.cpp" line="167"/>
+        <source>Times limit for failure[0 means no limit]</source>
+        <translation>失败次数限制[0表示无限制]</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="188"/>
+        <source>Seconds to wait on failure</source>
+        <translation>认证失败时等待时间</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="212"/>
+        <source>Interval between sending two heartbeat packages</source>
+        <translation>每两次发送心跳包之间的时间间隔</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="236"/>
+        <source>The version of authentification client to imitate[default to 0.00, compatible with xrgsu]</source>
+        <translation>模拟的客户端版本[默认0.00表示兼容xrgsu]</translation>
+    </message>
+    <message>
+        <source>Seconds to wait for authentication.</source>
+        <translation type="obsolete">等待认证完成的时间。</translation>
+    </message>
+    <message>
+        <location filename="../src/configwindow.cpp" line="146"/>
+        <location filename="../src/configwindow.cpp" line="191"/>
+        <location filename="../src/configwindow.cpp" line="215"/>
         <source>s</source>
         <translation>秒</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="157"/>
+        <location filename="../src/configwindow.cpp" line="166"/>
         <source>Max failure times:</source>
         <translation>允许失败次数:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="158"/>
         <source>Times limit for failure[0 means no limit].</source>
-        <translation>失败次数限制[0表示无限制]。</translation>
+        <translation type="obsolete">失败次数限制[0表示无限制]。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="178"/>
+        <location filename="../src/configwindow.cpp" line="187"/>
         <source>Wait on failure timeout:</source>
         <translation>失败等待:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="179"/>
         <source>Seconds to wait on failure.</source>
-        <translation>认证失败时等待时间。</translation>
+        <translation type="obsolete">认证失败时等待时间。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="202"/>
+        <location filename="../src/configwindow.cpp" line="211"/>
         <source>Heartbeat timeout:</source>
         <translation>心跳间隔:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="203"/>
         <source>Interval between sending two heartbeat packages.</source>
-        <translation>每两次发送心跳包之间的时间间隔。</translation>
+        <translation type="obsolete">每两次发送心跳包之间的时间间隔。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="226"/>
+        <location filename="../src/configwindow.cpp" line="235"/>
         <source>Client Version:</source>
         <translation>客户端版本号:</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="227"/>
         <source>The version of authentification client to imitate[default to 0.00, compatible with xrgsu].</source>
-        <translation>模拟的客户端版本[默认0.00表示兼容xrgsu]。</translation>
+        <translation type="obsolete">模拟的客户端版本[默认0.00表示兼容xrgsu]。</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="248"/>
+        <location filename="../src/configwindow.cpp" line="258"/>
         <source>Show balloon message on system tray</source>
         <translation>在系统托盘显示气泡通知</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="269"/>
+        <location filename="../src/configwindow.cpp" line="286"/>
         <source>Configuration</source>
         <translation>配置</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="353"/>
+        <location filename="../src/configwindow.cpp" line="370"/>
         <source>Out of Range</source>
         <translation>超出范围</translation>
     </message>
     <message>
-        <location filename="../src/configwindow.cpp" line="353"/>
+        <location filename="../src/configwindow.cpp" line="370"/>
         <source>The second value should be 0-20.</source>
         <translation>时间值应在0-20范围。</translation>
     </message>
